0be36afe291049b1a2447a3c5878fce7f879c22c
mnannuri <mounika.n@apolloits.com>
2025-04-17 02:36:07 +0000

    CTOC Development

--- src/main/java/com/apolloits/util/repository/AgencyRepository.java
+++ src/main/java/com/apolloits/util/repository/AgencyRepository.java
@@ -2,6 +2,7 @@ package com.apolloits.util.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +15,7 @@ import com.apolloits.util.modal.AgencyEntity;
 public interface AgencyRepository extends JpaRepository<AgencyEntity, Integer> {
 	@Query("SELECT ag FROM AgencyEntity ag WHERE ag.CSCID = :CSCID ")
 	List<AgencyEntity> findByCSCIDforAgency(@Param("CSCID") String CSCID);
+	
+	@Query("SELECT a FROM AgencyEntity a WHERE a.HubName = :HubName")
+	List<AgencyEntity> findByHubName(@Param("HubName")String name, Sort sort);
 }
